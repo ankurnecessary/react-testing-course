e6f2319d9cc78973a478ca8e67beca86b58a0d54
Ankur Gupta <ankurnecessary@gmail.com>
2024-06-08 16:27:24 +0930

    Testing on the basis of testid instead of link text.

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,13 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByTestId('learn-link')
   expect(linkElement).toBeInTheDocument();
+});
+
+test('url is correct', () => {
+  render(<App />);
+  // Fetching a link on the basis of <a data-testid="learn-link" />
+  const linkElement = screen.getByTestId('learn-link');
+  expect(linkElement.href).toContain('ultimateqa.com');
 });
